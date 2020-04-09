e46bc60a6b49e577f3f07d85dd75dbf63c6026f3
CharterConstruction <35385997+CharterConstruction@users.noreply.github.com>
2020-04-09 15:38:24 -0700

    Assignment 1

---
+++ Assignment1/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EnableSubmitIfReadyToSubmit();
+
+            //this.WindowState = WindowState.Maximized;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password: " + uxPassword.Text);
+        }
+        
+
+        private bool IsReadyToSubmit()
+        {
+            if (IsTextBlockFilled(uxName) && IsTextBlockFilled(uxPassword))
+                return true;
+            else
+                return false;
+        }
+
+
+        private bool IsTextBlockFilled(TextBox targetTextBox)
+        {
+            return !String.IsNullOrWhiteSpace(targetTextBox.Text);
+        }
+
+
+        private void EnableSubmitIfReadyToSubmit()
+        {
+            uxSubmit.IsEnabled = IsReadyToSubmit();            
+        }
+
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitIfReadyToSubmit();
+        }
+
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitIfReadyToSubmit();
+        }
+
+    }
+}
