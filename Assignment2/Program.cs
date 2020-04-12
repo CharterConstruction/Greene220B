d9ae9ef3790c9fff4c6d0d081c3df98926dcd35b
CharterConstruction <35385997+CharterConstruction@users.noreply.github.com>
2020-04-12 15:24:53 -0700

    homework 2
    homework 2 as of 4/12/20

---
+++ Assignment2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using HelloWorld.Models;
+using System.Linq;
+
+
+namespace Assignment2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<User>();
+
+            users.Add(new User { Name = "Dave", Password = "hello" });
+            users.Add(new User { Name = "Steve", Password = "steve" });
+            users.Add(new User { Name = "Lisa", Password = "hello" });
+
+
+
+
+            //var remainingUsers = users.Select(t => $"Name: {t.Name}, Password {t.Password}");            
+            //string.Join(Environment.NewLine, remainingUsers);
+
+
+            // Display users with password == "hello"
+            WriteUsersWithPasswordHello(users);
+
+            // Remove users where password == user's name
+            users.RemoveAll(t => t.Password == t.Name.ToLower());
+
+            // Delete the first user w/ password hello
+            users.Remove(users.FindAll(t => t.Password == "hello").FirstOrDefault());
+
+            // Display remaining users
+            WriteUsersToConsole(users);
+
+
+          
+
+        }
+
+        public static void WriteUsersWithPasswordHello(List<User> listOfUsers)
+        {
+            var results = from u in listOfUsers
+                          where u.Password == "hello"
+                          select u.Name;
+
+            string outputString = "The Following Users Have 'hello' as Their Password:" + Environment.NewLine;
+            outputString += "=======================================================" + Environment.NewLine;
+            outputString += string.Join(Environment.NewLine, results);
+
+            Console.WriteLine(outputString);
+
+        }
+
+
+        public static void WriteUsersToConsole(List<User> listOfUsers)
+        {
+            var userNames = listOfUsers.Select(t => $"Name: {t.Name}, Password: {t.Password}");
+
+
+
+            string outputString = Environment.NewLine + "Current Users" + Environment.NewLine;
+            outputString += "=======================================================" + Environment.NewLine;
+            outputString += string.Join(Environment.NewLine, userNames) + Environment.NewLine;
+            outputString += "=======================================================" + Environment.NewLine;
+
+            Console.WriteLine(outputString);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ Class 2/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Models.User user = new Models.User(); 
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EnableSubmitIfReadyToSubmit();
+            uxName.DataContext = user;
+            uxNameError.DataContext = user;
+            uxPassword.DataContext = user;
+            uxPasswordError.DataContext = user;
+
+            //this.WindowState = WindowState.Maximized;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password: " + uxPassword.Text);
+        }
+        
+
+        private bool IsReadyToSubmit()
+        {
+            if (IsTextBlockFilled(uxName) && IsTextBlockFilled(uxPassword))
+                return true;
+            else
+                return false;
+        }
+
+
+        private bool IsTextBlockFilled(TextBox targetTextBox)
+        {
+            return !String.IsNullOrWhiteSpace(targetTextBox.Text);
+        }
+
+
+        private void EnableSubmitIfReadyToSubmit()
+        {
+            uxSubmit.IsEnabled = IsReadyToSubmit();            
+        }
+
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitIfReadyToSubmit();
+        }
+
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitIfReadyToSubmit();
+        }
+
+    }
+}
