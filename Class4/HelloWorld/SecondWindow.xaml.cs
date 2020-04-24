ae11f52b711fd35fe982f5c0f8fa8b44222a476f
CharterConstruction <35385997+CharterConstruction@users.noreply.github.com>
2020-04-23 18:03:11 -0700

    Class 4 Start

---
+++ Class4/HelloWorld/SecondWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HelloWorld.Models;
+
+namespace HelloWorld
+{
+
+    
+
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        private List<User> users = null;
+
+        public SecondWindow()
+        {
+            InitializeComponent();
+            InitializeUsers();
+            
+        }
+
+        private void uxList_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedColumn = (GridViewColumnHeader)e.OriginalSource;
+            ToggleSortByColumn(selectedColumn);
+
+        }
+
+
+        private void InitializeUsers()
+        {
+            users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "LisaPwd" });
+            users.Add(new Models.User { Name = "Abe", Password = "Xerox" });
+            users.Add(new Models.User { Name = "Jacob", Password = "Password123" });
+            users.Add(new Models.User { Name = "Chris", Password = "Money$$" });
+            users.Add(new Models.User { Name = "Adam", Password = "Oriole" });
+            users.Add(new Models.User { Name = "Yinzer", Password = "Pittsburgh" });
+
+            uxList.ItemsSource = users;
+
+        }
+
+
+
+        private void ToggleSortByColumn(GridViewColumnHeader columnHeader)
+        {
+            string columnName = columnHeader.Content.ToString();            
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(users);         
+            ListSortDirection sortDirection = ListSortDirection.Ascending;
+            
+            if (view.SortDescriptions.Count > 0)
+            {
+                string currentColumn = view.SortDescriptions[0].PropertyName.ToString();
+                ListSortDirection currentSortDirection = view.SortDescriptions[0].Direction;
+                
+                if (currentColumn == columnName && currentSortDirection == ListSortDirection.Ascending)
+                    sortDirection = ListSortDirection.Descending;
+            }
+                       
+            //Clear and set the target column's sorting.
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription(columnHeader.Content.ToString(), sortDirection));
+        }
+
+
+
+
+
+    }
+}
