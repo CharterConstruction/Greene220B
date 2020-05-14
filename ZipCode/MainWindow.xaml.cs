a1e42d2dbb435178b94e31ada0ca3aaaaae19a85
CharterConstruction <35385997+CharterConstruction@users.noreply.github.com>
2020-05-13 18:13:29 -0700

    Homework 4

--- ZipCode/MainWindow.xaml.cs
+++ ZipCode/MainWindow.xaml.cs
@@ -23,27 +23,42 @@ namespace ZipCode
     public partial class MainWindow : Window
     {
         private const string _usZipPattern = "^(\\d{5}(?:\\-\\d{4})?)$";
+        private const string _canadaPostalPattern = "^([A-Za-z]\\d[A-Za-z][-]?\\d[A-Za-z]\\d)";
 
+        	
         public MainWindow()
         {
             InitializeComponent();
+            uxSubmit.IsEnabled = false;
         }
 
-        private void uxZipCode_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void uxZipCode_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textBox = e.Source as TextBox;
             Match matchesUSPostal = Regex.Match(textBox.Text, _usZipPattern);
-            Match matchesCanadaPostal = Regex.Match(textBox.Text, _usZipPattern);
+            Match matchesCanadaPostal = Regex.Match(textBox.Text, _canadaPostalPattern);
 
-            if(matchesUSPostal.Success|| matchesCanadaPostal.Success)
+            if (matchesUSPostal.Success)
             {
-                uxStatusMessage.Text = "Match!";
+                uxStatusMessage.Text = "Appears to be a valid US Zip Code!";
+                uxSubmit.IsEnabled = true;
+            }
+            else if (matchesCanadaPostal.Success)
+            {
+                uxStatusMessage.Text = "Appears to be a valid Canadian Postal Code!";
+                uxSubmit.IsEnabled = true;
             }
             else
             {
-                uxStatusMessage.Text = "FAIL";
+                uxStatusMessage.Text = "Please enter a valid US Zip Code or Canadian Postal Code";
+                uxSubmit.IsEnabled = false;
             }
 
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
