dac6ee77cb881225a58536a21c010497c4edac34
CharterConstruction <35385997+CharterConstruction@users.noreply.github.com>
2020-05-28 19:56:41 -0700

    Class 9

---
+++ Class9/HelloWorld/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please enter an email")]
+        public string Email { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
